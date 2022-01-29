f98bd34f36be15d09ffe82295363f26c16d8f01c
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2022-01-29 22:31:34 +0530

    Add files via upload

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Subtotal.css";
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
     const [{ basket }, dispatch] = useStateValue();
@@ -11,8 +12,7 @@ function Subtotal() {
                 renderText={(value) => (
                     <>
                         <p>
-                            {/* homework */}
-                            Subtotal ({basket.length} items): <strong>0</strong>
+                            Subtotal ({basket.length} items): <strong>{value}</strong>
                         </p>
                         <small className='subtotal__gift'>
                             <input type="checkbox" /> This order contains a gift
@@ -20,7 +20,8 @@ function Subtotal() {
                     </>
                 )}
                 decimalScale={2}
-                value={0}  // homework
+                value={getBasketTotal(basket)}  // homework
+                thousandSpacing='2s'
                 thousandSeparator={true}
                 displayType={"text"}
                 prefix={"₹"}
